3422733370d697ff51b5ca13af75fc6d6d8a9e2e
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-02-05 19:24:30 -0300

    Desenvolvimento das rotas com controllers

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Alessandro Rodrigo',
-    email: 'alessandro.perez@etec.sp.gov.br',
-    password_hash: '123',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
