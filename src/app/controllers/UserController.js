e225ab1e62299c7d811ba8c0d24accc026857436
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-02-05 19:24:11 -0300

    Controller e model User

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,58 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists!' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const userUpdated = await User.update(req.body, {
+      where: { id: req.userId },
+      returning: true,
+    });
+
+    const { name, provider, id } = userUpdated[1][0];
+
+    // return res.send(userUpdated);
+
+    return res.json({
+      name,
+      email,
+      provider,
+      id,
+    });
+  }
+}
+
+export default new UserController();
